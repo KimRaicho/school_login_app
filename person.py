b9eae5c80f62c7fd61a02c510050343e3dde6f72
Kimraicho <evansmuthugumi@gmail.com>
2021-10-14 23:00:09 +0300

    Created person class
    class that creates a person instance

---
+++ person.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, name: str, age: int = 0):
+        assert age >= 0, f"Age {age} should be greater or equal to 0"
+
+        self.name = name
+        self.age = age
+
+    def person_details_printer(self):
+        print(f"""
+Name: {self.name}
+Age:  {self.age}
+        """)
+
+
+evans = Person("Evans", 29)
+jane = Person("Jane", 32)
